4408784feceee0e816acbe133c55f0c9c7ab48bb
Marijn Goedegebure <marijn@fluxility.nl>
2016-01-03 15:34:48 +0100

    Added sql injection vulnerability feature

--- unsafe_project/unsafe/auth_backend.py
+++ unsafe_project/unsafe/auth_backend.py
@@ -5,7 +5,9 @@ from django.contrib.auth.models import User
 class UnsafeAuthBackend(object):
     def authenticate(self, username=None, password=None):
         # Check the username/password and return a User.
-        unsafe_user = UnsafeUser.objects.get(user__username=username, plaintext_password=password)
+        sql = "SELECT * FROM auth_user au, unsafe_unsafeuser uu WHERE au.id = uu.user_id AND au.username='{0}' AND uu.plaintext_password='{1}'".format(username, password)
+        unsafe_user_query_set = UnsafeUser.objects.raw(sql)
+        unsafe_user = unsafe_user_query_set[0]
         return unsafe_user.user
 
     def get_user(self, user_id):
